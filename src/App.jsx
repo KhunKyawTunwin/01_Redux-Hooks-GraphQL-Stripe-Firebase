10e5ca73cdb1331f8838a2795f92de7e68395df0
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-05-18 07:59:01 +0630

    React Full

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return {
+            monsters: users,
+          };
+        })
+      );
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          {this.state.monsters.map((monster) => {
+            return <h1 key={monster.id}>{monster.name}</h1>;
+          })}
+        </div>
+      </>
+    );
+  }
+}
+export default App;
+
+// https://jsonplaceholder.typicode.com/users
