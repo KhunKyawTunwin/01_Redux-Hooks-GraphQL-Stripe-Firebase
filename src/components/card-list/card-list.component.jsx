b0c48f470c9e57c3eda3121b6b17b1e6712de25c
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-06-07 14:25:03 +0630

    client

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
+
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => {
+          return (
+            <div className="card-container" key={monster.id}>
+              <img
+                src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+                alt=""
+              />
+              <h1>{monster.name}</h1>
+              <p>{monster.email}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default CardList;
